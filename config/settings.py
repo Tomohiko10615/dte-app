ec13cecca272ddf06e4caed0c866437e2146e326
Tomohiko10615 <richard.inafuko@unmsm.edu.pe>
2021-05-22 13:55:40 -0500

    Heroku updates

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd%0mv^*jm_rc#$yq-&#r@y#7@d#+dq(zjli5qm51r@-0mn$+s0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
